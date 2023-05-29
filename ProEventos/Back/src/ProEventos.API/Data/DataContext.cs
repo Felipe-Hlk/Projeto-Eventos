422eeb7d50eb0e0fac4607815322675a1db034d0
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-05-29 15:00:07 -0300

    Create Data.Context and update Startup.cs

--- ProEventos/Back/src/ProEventos.API/Data/DataContext.cs
+++ ProEventos/Back/src/ProEventos.API/Data/DataContext.cs
@@ -2,11 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.Entity.FrameworkCore;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Data
 {
-    public class DataContext : 
+    public class DataContext : DbContext
     {
-        
+        public DbSet<Evento> Eventos { get; set; }      
     }
 }
