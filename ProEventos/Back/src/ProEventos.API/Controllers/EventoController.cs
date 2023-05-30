3974f9edb3e4aff14701757de1b73d291dadb7ff
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-05-30 15:17:45 -0300

    Update EventoController

--- ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
+++ ProEventos/Back/src/ProEventos.API/Controllers/EventoController.cs
@@ -5,41 +5,31 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ProEventos.API.Models;
+using ProEventos.API.Data;
 
 namespace ProEventos.API.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
-    {
-        private IEnumerable<Evento> _eventos = new List<Evento>
+    {      
+        private readonly DataContext _context;
+
+        public EventoController(DataContext context)
         {
-            new Evento
-            {
-                EventoId = 1,
-                Local = "Campo Grande",
-                DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyyy"),
-                Tema = "Angular e .NET 5",
-                QtdPessoas = 250,
-                Lote = "1º Lote",
-                ImagemUrl = "foto.png"
-            },
-            new Evento
-            {
-                EventoId = 2,
-                Local = "São Paulo",
-                DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyyy"),
-                Tema = "Angular e suas Novidades",
-                QtdPessoas = 450,
-                Lote = "2º Lote",
-                ImagemUrl = "foto2.png"
-            }
-        };
+            _context = context;
+        }
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return _eventos;
+            return _context.Eventos;
+        }
+
+        [HttpGet("{id}")]
+        public Evento GetById(int id)
+        {
+            return _context.Eventos.FirstOrDefault(e => e.EventoId == id);
         }
     }
 }
