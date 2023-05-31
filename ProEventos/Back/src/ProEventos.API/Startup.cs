f49962a21dce664982493fe515a0956c1febae18
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-05-31 15:39:09 -0300

    config method Crors to permit to permit requisintion in html

--- ProEventos/Back/src/ProEventos.API/Startup.cs
+++ ProEventos/Back/src/ProEventos.API/Startup.cs
@@ -31,6 +31,7 @@ namespace ProEventos.API
                 options.UseSqlite(Configuration.GetConnectionString("Default")));
 
             services.AddControllers();
+            services. AddCors();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ProEventos.API", Version = "v1" });
@@ -49,6 +50,9 @@ namespace ProEventos.API
             app.UseHttpsRedirection();
             app.UseRouting();
             app.UseAuthorization();
+            app.UseCors(x => x.AllowAnyHeader()
+                              .AllowAnyMethod()
+                              .AllowAnyOrigin());
 
             app.UseEndpoints(endpoints =>
             {
