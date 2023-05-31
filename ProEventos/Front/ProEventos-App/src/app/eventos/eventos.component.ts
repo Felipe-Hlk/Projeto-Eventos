a14a46e2930c53b54d61dff7c609705258bb52d4
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-05-31 14:35:54 -0300

    using ng for

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
+  public eventos: any =[
+  {
+    Tema: 'Angular',
+    Local: 'Campo Grande-MS'
+  },
+  {
+    Tema: 'React',
+    Local: 'Belo Horizonte-MG'
+  },
+  {
+    Tema: '.NET',
+    Local: 'São Paulo-SP'
+  },
+  {
+    Tema: 'Docker',
+    Local: 'Florianópolis-SC'
+  },
+]
+
   constructor() { }
 
   ngOnInit(): void {
