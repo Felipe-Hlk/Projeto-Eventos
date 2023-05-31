39d549fcca334fefce7cb8fbce74eb2a52de4d4f
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-05-31 15:08:56 -0300

    import HttpClientModules

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -7,28 +7,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any =[
-  {
-    Tema: 'Angular',
-    Local: 'Campo Grande-MS'
-  },
-  {
-    Tema: 'React',
-    Local: 'Belo Horizonte-MG'
-  },
-  {
-    Tema: '.NET',
-    Local: 'São Paulo-SP'
-  },
-  {
-    Tema: 'Docker',
-    Local: 'Florianópolis-SC'
-  },
-]
+  public eventos: any; // eventos declarada, que recebera a ligação através da interpolação no html
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { //ngOnInit executa antes da requisição pro html
+    this.geteventos();// devido a funão do ngOninit getEventos precisa ser inserido após ser declarado, caso contrário não aprecerá na aplicação
   }
 
+  /* O objetivo de encapsular a lógica de população do array eventos dentro da função geteventos()
+    é separar a obtenção dos dados da lógica de inicialização do componente. Essa abordagem é útil
+    quando você precisa buscar dados de forma assíncrona, como por meio de uma requisição HTTP,
+    o que será feito nessa atividade, os dados dos eventos serão obtidos de um serviço backend
+    através de uma requisição HTTP, a função geteventos() irá conter a chamada ao serviço e
+    a manipulação da resposta para preencher o array eventos. Dessa forma, separar essa lógica
+    em um método separado permite manter o código mais organizado e reutilizável. As informações em
+    getEventos irão ser buscadas em um banco de dados, não sendo assim estáticas.
+  */
+
+  public geteventos(): void {
+
+    this.eventos = [
+      {
+        Tema: 'Angular',
+        Local: 'Campo Grande-MS'
+      },
+      {
+        Tema: 'React',
+        Local: 'Belo Horizonte-MG'
+      },
+      {
+        Tema: '.NET',
+        Local: 'São Paulo-SP'
+      },
+      {
+        Tema: 'Docker',
+        Local: 'Florianópolis-SC'
+      },
+    ];
+
+  }
 }
