c6af295a95d20c9a2d32eefc99e804035d04f69c
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-02 00:08:36 -0300

    change images

--- ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,26 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any; // eventos declarada, que recebera a ligação através da interpolação no html
+  // isCollapsed = true;
 
-  constructor(private http: HttpClient) { } // HttpCliente inserido no constructor obs: variável de no 'http' após 'private'
+  public eventos: any = []; // eventos declarada, que recebera a ligação através da interpolação no html
+
+  constructor(private http: HttpClient) { } // HttpCliente inserido no constructor obs: variável de nome 'http' após 'private'
 
   ngOnInit(): void { //ngOnInit executa antes da requisição pro html
     this.geteventos();// devido a funão do ngOninit getEventos precisa ser inserido após ser declarado, caso contrário não aprecerá na aplicação
   }
 
-  /* O objetivo de encapsular a lógica de população do array eventos dentro da função geteventos()
+  /* O objetivo de encapsular a lógica de programção do array eventos dentro da função geteventos()
     é separar a obtenção dos dados da lógica de inicialização do componente. Essa abordagem é útil
     quando você precisa buscar dados de forma assíncrona, como por meio de uma requisição HTTP,
     o que será feito nessa atividade, os dados dos eventos serão obtidos de um serviço backend
     através de uma requisição HTTP, a função geteventos() irá conter a chamada ao serviço e
     a manipulação da resposta para preencher o array eventos. Dessa forma, separar essa lógica
     em um método separado permite manter o código mais organizado e reutilizável. As informações em
-    getEventos irão ser buscadas em um banco de dados, não sendo assim estáticas.
+    getEventos serão buscadas em um banco de dados, não sendo assim estáticas.
   */
 
   public geteventos(): void {
-
     this.http.get( ' https://localhost:5001/api/Eventos ' ).subscribe(
       response => this.eventos = response,
       error => console.log (error)
