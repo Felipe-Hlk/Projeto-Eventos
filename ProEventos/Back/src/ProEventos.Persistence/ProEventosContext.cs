b341bca02c6a960dee29c1bd2e258b527575fa15
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-05 17:18:09 -0300

    update references

--- ProEventos/Back/src/ProEventos.Persistence/ProEventosContext.cs
+++ ProEventos/Back/src/ProEventos.Persistence/ProEventosContext.cs
@@ -18,5 +18,14 @@ namespace ProEventos.Persistence
         public DbSet<Palestrante> Palestrantes { get; set; }
         public DbSet<PalestranteEvento> PalestrantesEventos{ get; set; }
         public DbSet<RedeSocial> RedesSociais { get; set; }
+
+        /* abaixo associação feita de mn entre 'Eventos'e 'Palestrantes' pois empre irá
+        receber 'PalestrantesEventos', vinculando as chaves de um para o outro, consolidando
+        a classe 'PalestrantesEventos' como a classe de junção entre 'Eventos'e 'Palestrantes' */
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<PalestranteEvento>()
+                .HasKey(PE => new { PE.EventoId, PE.PalestranteId });
+        }
     }
 }
