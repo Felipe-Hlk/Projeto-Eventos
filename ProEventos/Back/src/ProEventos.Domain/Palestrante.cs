d23a7ee812c7e1df121b44d7be389e483cd5ce8d
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-05 16:04:00 -0300

    linking the classes to the database
    including mn link between palestrantes and eventos

--- ProEventos/Back/src/ProEventos.Domain/Palestrante.cs
+++ ProEventos/Back/src/ProEventos.Domain/Palestrante.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace ProEventos.Domain.Models
+namespace ProEventos.Domain
 {
-    public class Evento
+    public class Palestrante
     {
         public int Id { get; set; }
-        public string Local { get; set; }
-        public DateTime? DataEvento { get; set; }
-        public string Tema { get; set; }
-        public int QtdPessoas { get; set; }
+        public string Nome { get; set; }
+        public string MiniCurriculo { get; set; }
+        public string ImagemUrl { get; set; }
         public string Telefone { get; set; }
         public string Email { get; set; }
-        public string ImagemUrl { get; set; }
-        public IEnumerable<lote> Lote { get; set; }
         public IEnumerable<RedeSocial> RedesSociais { get; set; }
         public IEnumerable<PalestranteEventos> PalestranteEventos { get; set; }
     }
