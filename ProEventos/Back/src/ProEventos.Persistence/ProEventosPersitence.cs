536949c8eadc2bbc0764bd9e418025afb94f1f97
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-07 18:37:54 -0300

    Changing ProEventosPersistence.cs

--- ProEventos/Back/src/ProEventos.Persistence/ProEventosPersitence.cs
+++ ProEventos/Back/src/ProEventos.Persistence/ProEventosPersitence.cs
@@ -2,45 +2,96 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 
 namespace ProEventos.Persistence
 {
     public class ProEventosPersitence : IProEventosPersistence
     {
+        private readonly ProEventosContext _context;
+        public ProEventosPersitence(ProEventosContext _context)
+        {
+            this._context = _context;
+        }
         public void Add<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Add(entity);
         }
         public void Update<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Update(entity);
         }
         public void Delete<T>(T entity) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.Remove(entity);
         }
-        public void DeleteRange<T>(T entity) where T : class
+        public void DeleteRange<T>(T[] entityArray) where T : class
         {
-            throw new System.NotImplementedException();
+            _context.RemoveRange(entityArray);
         }
 
         public Task<bool> SaveChangesAsync()
         {
-            throw new System.NotImplementedException();
+            return (await _context.SaveChangesAsync()) > 0;
         }
 
         //EVENTOS
-        public Task<Evento[]> GetAllEventosByTemaAsync(string tema, bool includePalestrantes)
+        public async Task<Evento[]> GetAllEventosByTemaAsync(bool includePalestrantes = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Evento> query = _context.Eventos
+                .Include(e => e.Lotes)
+                .Include(e => e.RedesSociais);
+            
+
+            if (includePalestrantes){
+
+                query = query
+                    .Include(e => e.PalestrantesEventos)
+                    .ThenInclude(pe => pe.Palestrante);
+            }
+
+            query = query.OrderBy(e => e.Id);
+
+            return await query.ToArrayAsync();
+
         }
-        public Task<Evento[]> GetAllEventosAsync(string tema, bool includePalestrantes)
+        public async Task<Evento[]> GetAllEventosAsync(string tema, bool includePalestrantes = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Evento> query = _context.Eventos
+                .Include(e => e.Lotes)
+                .Include(e => e.RedesSociais);
+            
+
+            if (includePalestrantes){
+
+                query = query
+                    .Include(e => e.PalestrantesEventos)
+                    .ThenInclude(pe => pe.Palestrante);
+            }
+
+            query = query.OrderBy(e => e.Id) 
+                         .Where(e => e.Tema.ToLower().Contains(tema.ToLower));
+
+            return await query.ToArrayAsync();
         }
-        public Task<Evento[]> GetEventosByIdAsync(int EventoId, bool includePalestrantes)
+        public async Task<Evento> GetEventosByIdAsync(int EventoId, bool includePalestrantes = false)
         {
-            throw new System.NotImplementedException();
+            IQueryable<Evento> query = _context.Eventos
+                .Include(e => e.Lotes)
+                .Include(e => e.RedesSociais);
+            
+
+            if (includePalestrantes){
+
+                query = query
+                    .Include(e => e.PalestrantesEventos)
+                    .ThenInclude(pe => pe.Palestrante);
+            }
+
+            query = query.OrderBy(e => e.Id) 
+                         .Where(e => e.Id == EventoId);
+
+            return await query.FirstOrDefaultAsync();
         }
 
         //PALESTRANTES
