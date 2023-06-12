01660dcb9d51b590c3b897b26c446a6ebecaa4c3
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-12 16:47:16 -0300

    creating and setting angular interfaces
    establishing connection with domain in back folder

---
+++ ProEventos/Front/ProEventos-App/src/app/models/Palestrante.ts
@@ -0,0 +1,15 @@
+import { Evento } from "./evento";
+import { RedeSocial } from "./rede-social";
+
+export interface Palestrante{
+
+    id: number;
+    nome: string;
+    miniCurriculo: string;
+    imagemURL: string;
+    telefone: string;
+    email: string;
+    redesSociais: RedeSocial;
+    palestrantesEventos: Evento;
+
+}
--- ProEventos/Front/ProEventos-App/src/app/models/evento.ts
+++ ProEventos/Front/ProEventos-App/src/app/models/evento.ts
@@ -1,2 +1,18 @@
+import { Palestrante } from "./Palestrante";
+import { Lote } from "./lote";
+import { RedeSocial } from "./rede-social";
+
 export interface Evento {
+
+    id: number;
+    local: string;
+    dataEvento?: Date;
+    tema: string;
+    qtdPessoas: number;
+    imagemURL: string;
+    telefone: string;
+    email: string;
+    lotes: Lote[];
+    redesSociais: RedeSocial[];
+    palestrantesEventos: Palestrante[];
 }
