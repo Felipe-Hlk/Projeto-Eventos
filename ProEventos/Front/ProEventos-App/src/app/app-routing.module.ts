ce31d5bd27170c0531a6b13354475ad2d3430d54
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-06-27 17:33:54 -0300

    add routes

--- ProEventos/Front/ProEventos-App/src/app/app-routing.module.ts
+++ ProEventos/Front/ProEventos-App/src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { EventosComponent } from './Components/eventos/eventos.component';
+import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { PalestrantesComponent } from './Components/palestrantes/palestrantes.component';
+import { PerfilComponent } from './Components/perfil/perfil.component';
+import { ContatosComponent } from './Components/contatos/contatos.component';
+
+const routes: Routes = [
+  {path: 'eventos', component: EventosComponent},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'palestrantes', component: PalestrantesComponent},
+  {path: 'perfil', component: PerfilComponent},
+  {path: 'contatos', component: ContatosComponent},
+  {path: '', redirectTo: 'eventos', pathMatch: 'full'},
+  {path: '**', redirectTo: 'eventos', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
