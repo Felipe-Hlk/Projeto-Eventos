9ac0d8fd99dda45b3104c6a3591383791c9ef646
Felipe Arruda <114841507+Felipe-Hlk@users.noreply.github.com>
2023-07-05 17:27:20 -0300

    config restive forms, mustmatch to set confirm password and interact buttons - perfil page

--- ProEventos/Front/ProEventos-App/src/app/Components/user/perfil/perfil.component.ts
+++ ProEventos/Front/ProEventos-App/src/app/Components/user/perfil/perfil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControlOptions, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ValidatorField } from 'src/app/helpers/validator-field';
 
 @Component({
   selector: 'app-perfil',
@@ -7,9 +9,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilComponent implements OnInit {
 
-  constructor() { }
+  form!: FormGroup;
 
+  get f(): any {
+    return this.form.controls;
+  }
+
+  constructor( private fb: FormBuilder) { }
   ngOnInit(): void {
+    this.validation();
+  }
+
+  private validation(): void {
+
+    const formOptions: AbstractControlOptions = {
+      validators: ValidatorField.MustMatch('senha', 'confirmarSenha')
+    };
+
+    this.form = this.fb.group ({
+      titulo: ['', [Validators.required,]],
+      primeiroNome: ['', [Validators.required,]],
+      ultimoNome: ['', [Validators.required,]],
+      email: ['', [Validators.required, Validators.email]],
+      telefone: ['', [Validators.required,]],
+      funcao: ['', [Validators.required,]],
+
+      senha: ['', [Validators.required, Validators.minLength(5)]],
+      confirmarSenha: ['', [Validators.required,]]
+
+    }, formOptions);
+  }
+
+  public resetForm(): void{
+    this.form.reset();
   }
 
 }
